aa1f852f7633fcb9e5d7c089a0576b3fa1d9bc4f
cai-he <2462389854@.com>
2021-05-11 12:38:07 +0800

    移动端初始化

---
+++ web/src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('../views/App.vue'),
+  }
+]
+const router = new VueRouter({
+  routes
+})
+
+export default router
